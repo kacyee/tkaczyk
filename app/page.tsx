d7f39c1efe42f3b6fb61a7d93fb52d8a0694be6b
Patryk Puslecki <patrykpuslecki@gmail.com>
2023-02-25 16:38:37 +0100

    remove destroy instance

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,6 @@ export default function Home() {
               options={{
                 afterComplete: (instance: any) => {
                   setTitleFinished(true);
-                  if (instance) return instance.destroy();
                   return;
                 },
               }}
