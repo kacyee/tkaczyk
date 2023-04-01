51b01dc6f07d95185607dde6bd4b29bf82ba3880
Patryk Puslecki <patrykpuslecki@gmail.com>
2023-04-01 14:07:03 +0200

    add kontakt page

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,15 +1,166 @@
-import Image from "next/image";
+"use client";
+import { motion } from "framer-motion";
+import { FormEventHandler, useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef<HTMLFormElement>(null);
+  const [emailSent, setEmailSent] = useState<boolean>(false);
+  const sendEmail = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_ovz64l8",
+        "template_6tizw0k",
+        form.current!,
+        "q0vClDD5NP9_dCLUk"
+      )
+      .then(
+        (result) => {
+          if (result.status === 200) {
+            setEmailSent(true);
+            form.current!.reset();
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+  useEffect(() => {
+    if (emailSent) {
+      setTimeout(() => setEmailSent(false), 5000);
+    }
+  }, [emailSent]);
+
   return (
-    <main className="h-screen">
-      <Image
-        src="/images/Logo-black.svg"
-        width="67"
-        height="51"
-        alt="Logo Paweł Tkaczyk"
-      />
-      <h1>Contact!</h1>
+    <main className="relative z-[11] h-screen overflow-hidden xl:ml-[346px] xl:mr-[90px]  2xl:ml-[788px] 2xl:mr-[459px]">
+      <div className="grid h-full grid-cols-2">
+        <div className="flex flex-col border-r border-white uppercase">
+          <motion.div
+            initial={{ translateX: -40, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.4, delay: 0.7 }}
+          >
+            <h2 className="font-bold  text-black xl:mt-[175px] xl:mb-[70px] xl:text-[64px] 2xl:mt-[394px]">
+              Kontakt
+            </h2>
+          </motion.div>
+          <motion.nav
+            initial={{ translateX: -30, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 1 }}
+          >
+            <a
+              href="mailto:paweltkaczyk.design@gmail.com"
+              className="text-lg font-semibold transition duration-300 hover:text-white"
+            >
+              paweltkaczyk.design@gmail.com
+            </a>
+          </motion.nav>
+          <motion.nav
+            initial={{ translateX: -30, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 1.1 }}
+            className="mt-[30px]"
+          >
+            <a
+              className=" text-lg font-semibold transition duration-300 hover:text-white"
+              href="https://www.behance.net/pawulontkaczyk"
+            >
+              Behance
+            </a>
+          </motion.nav>
+          <motion.nav
+            initial={{ translateX: -30, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 1.2 }}
+            className="mt-[30px]"
+          >
+            <a
+              className="mt-[30px] text-lg font-semibold transition duration-300 hover:text-white"
+              href="https://dribble.com"
+            >
+              Dribble
+            </a>
+          </motion.nav>
+        </div>
+        <div className="flex flex-col  uppercase xl:pl-[70px] 2xl:pl-[230px]">
+          <motion.div
+            initial={{ translateX: -40, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.4, delay: 0.7 }}
+          >
+            <h2 className="w-max font-bold leading-none text-white xl:mt-[175px] xl:mb-[70px] xl:text-[64px] 2xl:mt-[394px]">
+              Prześlij
+              <span className="block text-right text-[26px]">pytanie</span>
+            </h2>
+          </motion.div>
+          <motion.nav
+            initial={{ translateX: -30, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 1.1 }}
+          >
+            <form ref={form} onSubmit={sendEmail} className="flex flex-col">
+              <input
+                type="text"
+                name="user_name"
+                className="mb-4 border-b border-white bg-transparent py-2 uppercase text-white placeholder-white outline-none"
+                placeholder="imię"
+              />
+              <input
+                type="email"
+                name="user_email"
+                className="mb-4 border-b border-white bg-transparent py-2 uppercase text-white placeholder-white outline-none"
+                placeholder="EMAIL"
+              />
+              <label className="mb-2 text-white">Wiadomość</label>
+              <textarea
+                name="message"
+                className="border border-white bg-transparent p-2 text-white outline-none xl:h-[180px] 2xl:h-[250px]"
+              />
+              <input
+                type="submit"
+                value="Wyślij"
+                className="ml-auto mt-4 w-max cursor-pointer border border-black bg-black px-6 py-2 text-lg text-white transition duration-300  hover:border-white hover:bg-transparent hover:text-white"
+              />
+            </form>
+            {emailSent ? (
+              <motion.nav
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "linear", duration: 0.5 }}
+              >
+                <div
+                  id="toast-simple"
+                  className="space-x  absolute top-[30px] right-0 flex w-full max-w-xs items-center space-x-4 divide-x divide-gray-200 rounded-lg bg-white p-4 text-gray-500 shadow dark:divide-gray-700 dark:text-black"
+                  role="alert"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="text-blue-600 dark:text-blue-500 h-5 w-5"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="paper-plane"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"
+                    ></path>
+                  </svg>
+                  <div className="pl-4 text-sm font-normal">
+                    Wiadomość wysłana!
+                  </div>
+                </div>
+              </motion.nav>
+            ) : null}
+          </motion.nav>
+        </div>
+      </div>
     </main>
   );
 }
