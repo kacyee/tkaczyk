ffafb40f1a36d78c7a3a2b7083b874fbbbdfed94
Patryk Puslecki <patrykpuslecki@gmail.com>
2023-04-26 15:29:46 +0200

    bigger fonts in mobile menu

--- components/MobileMenuItems.tsx
+++ components/MobileMenuItems.tsx
@@ -35,7 +35,7 @@ const MobileMenuItems = ({
             onClick={() => setActivePage("/")}
             className="flex h-full w-full items-center justify-center"
           >
-            <span className="uppercase">Strona główna</span>
+            <span className="text-xl uppercase">Strona główna</span>
           </Link>
         </motion.nav>
         <motion.nav
@@ -53,7 +53,7 @@ const MobileMenuItems = ({
             onClick={() => setActivePage(routes.PORTFOLIO)}
             className="flex h-full w-full items-center justify-center "
           >
-            <span className={`uppercase transition-all duration-500`}>
+            <span className={`text-xl uppercase transition-all duration-500`}>
               Portfolio
             </span>
           </Link>
@@ -73,7 +73,7 @@ const MobileMenuItems = ({
             onClick={() => setActivePage(routes.SERVICES)}
             className="flex h-full w-full items-center justify-center bg-yellow"
           >
-            <span className={`uppercase`}>Usługi</span>
+            <span className={`text-xl uppercase`}>Usługi</span>
           </Link>
         </motion.nav>
         <motion.nav
@@ -91,7 +91,7 @@ const MobileMenuItems = ({
             onClick={() => setActivePage(routes.CONTACT)}
             className="flex h-full w-full items-center justify-center"
           >
-            <span className={`uppercase`}>Kontakt</span>
+            <span className={`text-xl uppercase`}>Kontakt</span>
           </Link>
         </motion.nav>
       </div>
